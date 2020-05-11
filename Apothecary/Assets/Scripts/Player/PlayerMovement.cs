fba62ef02d02f171811fed21e1d876d50d1c2e82
bestfriend777 <59232292+bestfriend777@users.noreply.github.com>
2020-05-11 13:12:56 -0400

    Added MovementScript, Player Obj, Basic TileSet, Basic TileMap, and a sample scene

---
+++ Apothecary/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Dont need this for movement script
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This method is unreliable for physics because the update rate is variable based on our framerate
+        // Time and physics based code should go in a FixedUpdate() method
+
+        //
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // This is called by default 50x/per second
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime); 
+    }
+}
