4bf79a2965fe4f14feacdb87ef6e5fd005cd42a4
bestfriend777 <59232292+bestfriend777@users.noreply.github.com>
2020-06-06 22:00:52 -0400

    6/6/2020

--- Assets/Behaviors/ScriptableObjects/Inventory/Scripts/InventoryObject.cs
+++ Assets/Behaviors/ScriptableObjects/Inventory/Scripts/InventoryObject.cs
@@ -1,46 +1,90 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEditor;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Runtime.Versioning;
 
 [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
 
-public class InventoryObject : ScriptableObject
+public class InventoryObject : ScriptableObject, ISerializationCallbackReceiver
 {
+    public string savePath;
+    private ItemDatabaseObject database;
     public List<InventorySlot> inventoryContainer = new List<InventorySlot>();
+
+    private void OnEnable()
+    {
+#if UNITY_EDITOR
+        database = (ItemDatabaseObject)AssetDatabase.LoadAssetAtPath("Assets/Resources/Database.asset", typeof(ItemDatabaseObject));
+#else
+        database = Resources.Load<ItemDatabaseObject>("Database");
+#endif
+    }
 
     public void AddItem(ItemObject _item, int _amount)
     {
-        bool hasItem = false;
-
         // here if the InventoryContainer is size 0 then the for loop is skipped
         // maybe for each would work better
         for (int i = 0; i < inventoryContainer.Count; i++)
         {
-            if(inventoryContainer[i].item == _item)
+            if (inventoryContainer[i].item == _item)
             {
                 inventoryContainer[i].AddAmount(_amount);
-                hasItem = true;
-                break;
+                return;
             }
+        }
+        inventoryContainer.Add(new InventorySlot(database.GetId[_item], _item, _amount));
+    }
 
+    public void Save()
+    {
+        string saveData = JsonUtility.ToJson(this, true);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(string.Concat(Application.persistentDataPath, savePath));
+        bf.Serialize(file, saveData);
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if(File.Exists(string.Concat(Application.persistentDataPath, savePath)))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(string.Concat(Application.persistentDataPath, savePath), FileMode.Open);
+            JsonUtility.FromJsonOverwrite(bf.Deserialize(file).ToString(), this);
+            file.Close();
         }
 
-        if (!hasItem)
+    }
+
+    public void OnAfterDeserialize()
+    {
+        for (int i = 0; i < inventoryContainer.Count; i++)
         {
-            inventoryContainer.Add(new InventorySlot(_item, _amount));
-            UnityEngine.Debug.Log("didnt have the item. added to inventory");
+            inventoryContainer[i].item = database.GetItem[inventoryContainer[i].ID];
         }
     }
+
+    public void OnBeforeSerialize()
+    {
+
+    }
+
 }
 
 [System.Serializable]
 public class InventorySlot
 {
+    public int ID;
     public ItemObject item;
     public int amount;
 
-    public InventorySlot(ItemObject _item, int _amount)
+    public InventorySlot(int _ID, ItemObject _item, int _amount)
     {
+        ID = _ID;
         item = _item;
         amount = _amount;
     }
--- Assets/Behaviors/Scripts/Player/PlayerInventory.cs
+++ Assets/Behaviors/Scripts/Player/PlayerInventory.cs
@@ -1,10 +1,15 @@
-﻿ using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInventory : MonoBehaviour
 {
     public InventoryObject inventory;
+    public GameObject inventoryCanvas;
+
+    private Boolean isVisible = true;
 
     public void OnTriggerEnter2D(Collider2D otherCollider)
     {
@@ -13,8 +18,28 @@ public class PlayerInventory : MonoBehaviour
         if(pickupItem)
         {
             inventory.AddItem(pickupItem.item, 1);
-            UnityEngine.Debug.Log("Added item");    
             Destroy(otherCollider.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            inventory.Save();
+            UnityEngine.Debug.Log("Saved");
+
+        }
+        if (Input.GetKeyDown("l"))
+        {
+            inventory.Load();
+            UnityEngine.Debug.Log("Loaded");
+
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            //UIImage inventory = inventoryCanvas.GetComponentInChildren(UIImage, true);
+            //inventory.Image.enabled = false;
         }
     }
 
